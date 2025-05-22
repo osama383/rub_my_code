b662eb6bf7fc04690d8a6cd1d861219d866388f1
osama <sosamaalirizvi@gmail.com>
2025-05-22 12:40:10 -0600

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Rub my code'),
+      home: const MyHomePage(title: 'New version deployed!!!'),
     );
   }
 }
